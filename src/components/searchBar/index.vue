711a62b9da18057c831a7a5a95ac19b1a3f9297e
szx <2414126198@qq.com>
2019-11-12 21:00:29 +0800

    完成home页面，公共样式写到common.scss内，添加导航栏组件

---
+++ src/components/searchBar/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="search-eara">
+    <img src="@/assets/images/首页_slices/搜索@3x.png" alt />
+    <input type="text" :placeholder="ph" class="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ph: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.search-eara {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  border-radius: 20px;
+  background-color: #33363d;
+  img {
+    width: 17px;
+    margin: 0 6px 0 12px;
+  }
+  input {
+    flex: 1;
+    border: 0; // 去除未选中状态边框
+    outline: none; // 去除选中状态边框
+    background-color: #33363d;
+    color: #dfdfdf;
+  }
+}
+</style>
