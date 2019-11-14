01bdf703a549accc05863750e43eec1bd39e6797
szx <2414126198@qq.com>
2019-11-14 13:05:42 +0800

    给影院详情添加better-scroll

--- src/views/cinema/cinemaDetails/index.vue
+++ src/views/cinema/cinemaDetails/index.vue
@@ -1,53 +1,56 @@
 <template>
-  <div class="cinema-details">
+  <div class="cinema-details wrapper" ref="wrapper">
+    <div class="content">
+      <!-- 影院地址 -->
+      <router-link to="/cinema/cinemaMap" class="tomap" tag="div">
+        <img class="icon-l" src="@/assets/images/影院_slices/定位@3x.png" alt />
+        <span class="icon-r">&gt;</span>
+        <p class="name">{{cinemaDetails.title}}</p>
+        <p class="address">{{cinemaDetails.address}}</p>
+      </router-link>
+      <!-- 电影轮播列表 -->
+      <div class="cinema-list-swiper">
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="item in movieList" :key="item.id">
+            <img :src="imgUrl(item.img)" alt />
+          </swiper-slide>
+        </swiper>
+        <div class="movie-info">
+          <p class="movie-name">{{ movieList[currentMovie].name }}</p>
+          <p class="movie-details">{{ movieList[currentMovie].tag | tag }}</p>
+        </div>
+      </div>
+      <!-- 电影场次 -->
+      <div class="session">
+        <p class="today">今天2月1日</p>
+        <ul class="session-list">
+          <li class="session-item" v-for="item in 8" :key="item">
+            <div class="time">
+              <p>15:30</p>
+              <p>17:14散场</p>
+            </div>
+            <div class="movie-type">
+              <p>原版3D</p>
+              <p>2号厅(冠名招商中)</p>
+            </div>
+            <p class="movie-price">20.9元</p>
+            <div class="buy">购票</div>
+          </li>
+        </ul>
+      </div>
+    </div>
     <!-- nav开始 -->
     <nav-bar left="back" class="nav">
       <template v-slot:center>
         <p class="nav-title text-ellipsis">{{cinemaDetails.title}}</p>
       </template>
     </nav-bar>
-    <!-- 影院地址 -->
-    <router-link to="/cinema/cinemaMap" class="tomap" tag="div">
-      <img class="icon-l" src="@/assets/images/影院_slices/定位@3x.png" alt />
-      <span class="icon-r">&gt;</span>
-      <p class="name">{{cinemaDetails.title}}</p>
-      <p class="address">{{cinemaDetails.address}}</p>
-    </router-link>
-    <!-- 电影轮播列表 -->
-    <div class="cinema-list-swiper">
-      <swiper :options="swiperOption">
-        <swiper-slide v-for="item in movieList" :key="item.id">
-          <img :src="imgUrl(item.img)" alt />
-        </swiper-slide>
-      </swiper>
-      <div class="movie-info">
-        <p class="movie-name">{{ movieList[currentMovie].name }}</p>
-        <p class="movie-details">{{ movieList[currentMovie].tag | tag }}</p>
-      </div>
-    </div>
-    <!-- 电影场次 -->
-    <div class="session">
-      <p class="today">今天2月1日</p>
-      <ul class="session-list">
-        <li class="session-item" v-for="item in 8" :key="item">
-          <div class="time">
-            <p>15:30</p>
-            <p>17:14散场</p>
-          </div>
-          <div class="movie-type">
-            <p>原版3D</p>
-            <p>2号厅(冠名招商中)</p>
-          </div>
-          <p class="movie-price">20.9元</p>
-          <div class="buy">购票</div>
-        </li>
-      </ul>
-    </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+import BScroll from "@better-scroll/core";
 
 export default {
   components: {
@@ -149,13 +152,20 @@ export default {
     imgUrl(name) {
       return require(`@/assets/images/影院详情_slices/${name}`);
     }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        click: true
+      });
+    });
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .cinema-details {
-  padding-top: 44px;
+  height: 100%;
   background: url("~@/assets/images/影院详情_slices/椭圆形复制 3@3x.png")
     no-repeat center -40px / 100% auto;
   .nav {
@@ -165,93 +175,96 @@ export default {
     color: #fff;
     font-size: 16px;
   }
-  .tomap {
-    padding: 20px;
-    .icon-l {
-      width: 18px;
-      float: left;
-      margin-right: 25px;
-    }
-    .icon-r {
-      float: right;
-    }
-    .name {
-      font-size: 16px;
-      line-height: 22px;
-      color: #fff;
-    }
-    .address {
-      font-size: 12px;
-      line-height: 17px;
-    }
-  }
-  .cinema-list-swiper {
-    .swiper-slide {
-      width: 140px;
-      img {
-        width: 100%;
+  .content {
+    padding-top: 44px;
+    .tomap {
+      padding: 20px;
+      .icon-l {
+        width: 18px;
+        float: left;
+        margin-right: 25px;
+      }
+      .icon-r {
+        float: right;
+      }
+      .name {
+        font-size: 16px;
+        line-height: 22px;
+        color: #fff;
+      }
+      .address {
+        font-size: 12px;
+        line-height: 17px;
       }
     }
-    .movie-name {
-      padding-top: 10px;
-      padding-bottom: 4px;
-      font-size: 18px;
-      font-weight: 500;
-      text-align: center;
-      color: #fff;
-    }
-    .movie-details {
-      font-size: 14px;
-      text-align: center;
-    }
-  }
-  .session {
-    padding: 10px 20px;
-    .today {
-      font-size: 12px;
-      color: #fbc34a;
-    }
-    .session-list {
-      .session-item {
-        height: 91px;
-        background-color: #33363d;
-        margin-top: 10px;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
+    .cinema-list-swiper {
+      .swiper-slide {
+        width: 140px;
+        img {
+          height: 188px;
+        }
+      }
+      .movie-name {
+        padding-top: 10px;
+        padding-bottom: 4px;
+        font-size: 18px;
+        font-weight: 500;
         text-align: center;
-        line-height: 17px;
-        border-radius: 6px;
-        .time {
-          p {
-            font-size: 12px;
-            &:first-child {
-              color: #fff;
-              font-size: 18px;
-              line-height: 25px;
+        color: #fff;
+      }
+      .movie-details {
+        font-size: 14px;
+        text-align: center;
+      }
+    }
+    .session {
+      padding: 10px 20px;
+      .today {
+        font-size: 12px;
+        color: #fbc34a;
+      }
+      .session-list {
+        .session-item {
+          height: 91px;
+          background-color: #33363d;
+          margin-top: 10px;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          text-align: center;
+          line-height: 17px;
+          border-radius: 6px;
+          .time {
+            p {
+              font-size: 12px;
+              &:first-child {
+                color: #fff;
+                font-size: 18px;
+                line-height: 25px;
+              }
             }
           }
-        }
-        .movie-type {
-          width: 68px;
-          text-align: left;
-        }
-        .movie-price {
-          font-size: 18px;
-          font-weight: 500;
-          color: #fbc34a;
-          line-height: 25px;
-        }
-        .buy {
-          width: 45px;
-          line-height: 25px;
-          background: linear-gradient(
-            150deg,
-            rgba(242, 91, 134, 1) 0%,
-            rgba(241, 172, 94, 1) 100%
-          );
-          box-shadow: 0px 0px 4px 1px rgba(242, 109, 125, 0.18);
-          border-radius: 6px;
+          .movie-type {
+            width: 68px;
+            text-align: left;
+          }
+          .movie-price {
+            font-size: 18px;
+            font-weight: 500;
+            color: #fbc34a;
+            line-height: 25px;
+          }
+          .buy {
+            width: 45px;
+            line-height: 25px;
+            background: linear-gradient(
+              150deg,
+              rgba(242, 91, 134, 1) 0%,
+              rgba(241, 172, 94, 1) 100%
+            );
+            box-shadow: 0px 0px 4px 1px rgba(242, 109, 125, 0.18);
+            border-radius: 6px;
+          }
         }
       }
     }
